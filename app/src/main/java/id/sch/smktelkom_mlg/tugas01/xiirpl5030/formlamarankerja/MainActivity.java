839a05639cbb03f9006609581eafb8f29b4ae4f2
Ratri <Pratiwirespatiningratri@gmail.com>
2016-10-14 13:19:29 +0700

    Commit Validasi Error pada edit text

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5030/formlamarankerja/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5030/formlamarankerja/MainActivity.java
@@ -4,15 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
     Button bOk;
-    RadioButton radioButton, radioButton2, radioButton3;
+    CheckBox cb1, cb2, cb3, cb4;
+    RadioGroup rgStatus;
     TextView tvHasil;
+    Spinner SpPendidikan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,44 +27,62 @@ public class MainActivity extends AppCompatActivity {
         etNama = (EditText) findViewById(R.id.editTextNama);
         bOk = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        rgStatus = (RadioGroup) findViewById(R.id.RadioGroup);
+        cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        cb4 = (CheckBox) findViewById(R.id.checkBox4);
+        SpPendidikan = (Spinner) findViewById(R.id.SpinnerPendidikan);
 
+        kondisiawal();
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                doProcess();
-
+            public void onClick(View v) {
+                doClick();
             }
         });
     }
 
-    private boolean doProcess() {
-        if (isValid()) {
-            boolean valid = true;
-
-            String nama = etNama.getText().toString();
-            tvHasil.setText(nama);
-
-            if(nama.isEmpty())
-            {
-                etNama.setError("Nama belum diisi");
-                valid = false;
-            }
-            else if(nama.length()<3)
-            {
-                etNama.setError("Nama minimal 3 karakter");
-                valid = false;
-            }
-            else
-            {
-                etNama.setError(null);
-            }
-
-            return valid;
-        }
-        return false;
+    private void kondisiawal()
+    {
+        etNama.setText("");
+        cb1.setChecked(false);
+        cb2.setChecked(false);
+        cb3.setChecked(false);
+        cb4.setChecked(false);
+        rgStatus.clearCheck();
+        SpPendidikan.setSelection(0);
     }
 
-    private boolean isValid() {
-        return false;
+    private void doClick()
+    {
+        if(isValid())
+        {
+
+
+        }
+    }
+
+    private boolean isValid()
+    {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty())
+        {
+            etNama.setError("Nama Belum Diisi");
+            valid = false;
+        }
+        else if (nama.length()<3)
+        {
+            etNama.setError("Nama Minimal 3 karakter");
+            valid = false;
+        }
+        else
+        {
+            etNama.setError(null);
+        }
+        return valid;
     }
 }
